08d2c4356329f0820640309181dfbc3474faa809
memcmahon <mcmahon.meganelizabeth@gmail.com>
2024-06-17 13:56:22 -0600

    update search#index view

---
+++ spec/features/user_can_search_by_state_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature "user can search for members" do
+    scenario "user submits valid state name" do
+        visit '/'
+
+        save_and_open_page
+
+        select "Colorado", from: :state
+        click_on "Locate Representatives"
+        expect(current_path).to eq(search_path)
+
+        within(first(".member")) do
+            expect(page).to have_css(".name")
+            expect(page).to have_css(".party")
+            expect(page).to have_css(".state")
+        end
+    end
+end
